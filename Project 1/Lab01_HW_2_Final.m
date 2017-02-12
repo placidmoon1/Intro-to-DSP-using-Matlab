8dad24c1fa7b262a4db4d5997ced7b4062729ec9
placidmoon1 <placidmoon1@gmail.com>
2017-02-11 22:11:40 -0500

    Add files via upload

---
+++ Project 1/Lab01_HW_2_Final.m
@@ -0,0 +1,17 @@
+%Lab01_HW Q1 %Creation of audio file
+audObj1 = audiorecorder(8000,16,1);
+disp ('Start speaking.')
+recordblocking(audObj1, 5)
+disp('End of Recording.')
+audio = getaudiodata(audObj1);
+audiowrite('Q1.wav', audio, 8000);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%Lab01_HW Q2 %Segment of 5 periods
+xx = audioread('Q1.wav');
+ttA= 0.9:1/8000:0.93;
+xxA = xx(0.9*8000:1:(0.93)*8000);
+plot (ttA, xxA)
+grid on;
+title('Signal MAGNIFIED (0.9~0.93s) with Average Pitch Period of 49.4ms')
