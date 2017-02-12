1cf004f60bbbcf6c2f0de5a64a902707123fd0fc
placidmoon1 <placidmoon1@gmail.com>
2017-02-12 00:54:28 -0500

    Update makeLFMvals.m

--- Project 4/makeLFMvals.m
+++ Project 4/makeLFMvals.m
@@ -26,3 +26,11 @@ sigOut.times = tt;
 sigOut.values = xx;
 soundsc(xx, 1/dt)
 plotspec(xx, 1/dt), colorbar;
+
+% for testing: 
+% myLFMsig.f1 = 200;
+% myLFMsig.t1 = 0; myLFMsig.t2 = 1.5;
+% myLFMsig.slope = 1800;
+% myLFMsig.complexAmp = 10*exp(j*0.3*pi);
+% dt = 1/8000; % 8000 samples per sec is the sample rate
+% outLFMsig = makeLFMvals(myLFMsig,dt);
