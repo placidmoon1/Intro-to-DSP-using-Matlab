640b6b58c52a1b590eb21e350f6e76f42c6ce01c
placidmoon1 <placidmoon1@gmail.com>
2017-02-12 00:48:46 -0500

    Add files via upload

---
+++ Project 4/Lab04HW_Q1.m
@@ -0,0 +1,37 @@
+keynum = [45, 49, 52, 57, 52, 49, 45]; %key numbers of arp.
+amps = ones(1,length(keynum));
+freq = []; %initialize
+tStart = []; %initialize
+fs = 4000; %sampling rate is 4000Hz 
+tt = zeros (1, 4000*2.5+1); %initailzing time vector of duration 2.5s
+for i = 1:length(keynum)
+    freqKey = 440*(2^((keynum(i)-49)/12));
+    freq = [freq freqKey];
+end  
+for i = 1:length(keynum)
+    time = 0.25*(i-1); 
+    tStart = [tStart time]; 
+end 
+dur = [0.2, 0.2, 0.2]; %first three notes have dur of 0.2s
+for i = 4:length(keynum)
+    duration = 2.4 - tStart(i); %plays until 2.4s
+    dur = [dur duration]; %adding duration for 4th - 7th note
+end 
+for notes = 1:length(amps)
+    nStart = round(tStart(notes)*fs)+1; 
+    time = 0: 1/fs: dur(notes); 
+    xs = amps(notes) * cos( freq(notes)*2*pi*time ); %no phase needed
+    nStop = nStart + round(dur(notes)*fs);
+    tt(nStart:nStop) = tt(nStart:nStop) + xs;
+end 
+soundsc(tt, fs)
+plotspec(tt, fs, 256); grid on
+
+
+
+
+
+
+
+
+
---
+++ Project 4/makeLFMvals.m
@@ -0,0 +1,28 @@
+function sigOut = makeLFMvals( sigLFM, dt )
+% MAKELFMVALS generate a linear-FM chirp signal
+%
+% usage: sigOut = makeLFMvals( sigLFM, dt )
+% sigLFM.f1 = starting frequency (in Hz) at t = sigLFM.t1
+% sigLFM.t1 = starting time (in secs)
+% sigLFM.t2 = ending time
+% sigLFM.slope = slope of the linear-FM (in Hz per sec)
+% sigLFM.complexAmp = defines the amplitude and phase of the FM signal
+% dt = time increment for the time vector, typically 1/fs (sampling frequency)
+%
+% sigOut.values = (vector of) samples of the chirp signal
+% sigOut.times = vector of time instants from t=t1 to t=t2
+%
+if( nargin < 2 ) %-- Allow optional input argument for dt
+dt = 1/8000; %-- 8000 samples/sec
+end
+%--------NOTE: use the slope to determine mu needed in psi(t)
+%-------- use f1, t1 and the slope to determine f0 needed in psi(t)
+tt = 0:dt:sigLFM.t2;
+mu = sigLFM.slope/2;
+f0 = sigLFM.f1;
+psi = 2*pi*( f0*tt + mu*tt.*tt);
+xx = real( sigLFM.complexAmp * exp(j*psi) );
+sigOut.times = tt;
+sigOut.values = xx;
+soundsc(xx, 1/dt)
+plotspec(xx, 1/dt), colorbar;
