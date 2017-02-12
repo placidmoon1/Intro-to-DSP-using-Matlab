27869473a5d07f0bf53b9b6cc9e99949212f74ea
placidmoon1 <placidmoon1@gmail.com>
2017-02-11 22:17:01 -0500

    Update Creating and Analyzing Voice Waves.m

--- Project 1/Creating and Analyzing Voice Waves.m
+++ Project 1/Creating and Analyzing Voice Waves.m
@@ -8,7 +8,7 @@ audiowrite('Q1.wav', audio, 8000);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%Project 1-2 %Segment of 5 periods of the voice file 
+%Project 1-2 %Extracting segment of 5 periods of the voice file 
 xx = audioread('Q1.wav');
 ttA= 0.9:1/8000:0.93;
 xxA = xx(0.9*8000:1:(0.93)*8000);
